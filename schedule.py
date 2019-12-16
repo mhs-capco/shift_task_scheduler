3546d0554b2c7ded883381f8f38d2a48943d15f5
Mark Smith <mark.smith@capco.com>
2019-12-15 22:03:13 -0500

    adding main and test

---
+++ schedule.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+def welcome():
+    return "Welcome to the scheduler"
+
+if __name__ == "__main__":
+    print(welcome())
---
+++ schedule_test.py
@@ -0,0 +1,5 @@
+
+import schedule
+
+def test_welcome_msg():
+    assert schedule.welcome() == "Welcome to the scheduler"
